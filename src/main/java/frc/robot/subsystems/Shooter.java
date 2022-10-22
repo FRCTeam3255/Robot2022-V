b6dfea50481531a13aae79d9a8733a51869f9342
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-10-22 15:03:15 -0700

    removed allowable error from shooter config

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -47,9 +47,6 @@ public class Shooter extends SubsystemBase {
     config.slot0.kI = prefShooter.shooterI.getValue();
     config.slot0.kD = prefShooter.shooterD.getValue();
 
-    config.slot0.allowableClosedloopError = SN_Math.RPMToVelocity(
-        prefShooter.shooterAllowableClosedloopErrorRPM.getValue(), SN_Math.TALONFX_ENCODER_PULSES_PER_COUNT);
-
     leadMotor.configFactoryDefault();
     followMotor.configFactoryDefault();
 
