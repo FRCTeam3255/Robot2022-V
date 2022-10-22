a5176538e9e1bfaf6e0a8b16ff298443ff6a9bac
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-10-22 15:22:38 -0700

    Overloaded method fix (#49)
    closes #48

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -75,8 +75,7 @@ public class Hood extends SubsystemBase {
   }
 
   public void setAngleDegrees(SN_DoublePreference degrees) {
-    hoodMotor.set(ControlMode.Position, SN_Math.degreesToFalcon(degrees.getValue(), constHood.GEAR_RATIO),
-        DemandType.ArbitraryFeedForward, prefHood.hoodArbitraryFeedForward.getValue());
+    setAngleDegrees(degrees.getValue());
   }
 
   public boolean getBottomSwitch() {
