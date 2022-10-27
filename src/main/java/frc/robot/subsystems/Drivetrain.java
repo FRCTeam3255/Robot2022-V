ee3e62ad16e30e6794da0fea71823b0efd976277
Ian Szalai <60115484+IanSzalai@users.noreply.github.com>
2022-10-27 00:10:08 -0700

    removed more unrelated code

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -237,11 +237,8 @@ public class Drivetrain extends SubsystemBase {
   @Override
   public void periodic() {
     updatePose();
-    field.setRobotPose(getPose());
 
     if (displayOnDashboard) {
-
-      SmartDashboard.putData(field);
 
       SmartDashboard.putNumber("Drivetrain Left Encoder", leftLead.getSelectedSensorPosition());
       SmartDashboard.putNumber("Drivetrain Right Encoder", rightLead.getSelectedSensorPosition());
